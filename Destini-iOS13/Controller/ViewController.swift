ff062f04606b46960f3aa0900d7585ff2a83bb7e
Armando Lima <armandomattei1997@gmail.com>
2021-05-02 16:08:20 -0300

    MVC with destination of the story inside the array

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //OUTLETS
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+        
+    //VARIABLES
+    
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        storyLabel.text = storyBrain.stories[0].title
+        choice1Button.setTitle(storyBrain.stories[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[0].choice2, for: .normal)
     }
-
-
+    
+    //ACTIONS
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI(question: storyBrain.storyNumber)
+        
+    }
+    
+    //FUNCTIONS
+    
+    func updateUI(question: Int){
+        
+        storyLabel.text = storyBrain.stories[question].title
+        choice1Button.setTitle(storyBrain.stories[question].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[question].choice2, for: .normal)
+        
+    }
 }
 
